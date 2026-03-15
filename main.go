93f990f4cbab1b7c2e89243d54237fab3207c3a8
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2026-03-15 15:52:58 +0000

    List locations

--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 )
+
+type config struct {
+	Next     string
+	Previous string
+}
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(cfg *config) error
+}
+
+type locationArea struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type locationAreaResponse struct {
+	Count    int            `json:"count"`
+	Next     string         `json:"next"`
+	Previous string         `json:"previous"`
+	Results  []locationArea `json:"results"`
 }
 
 var cliCommands = map[string]cliCommand{
@@ -23,9 +44,24 @@ var cliCommands = map[string]cliCommand{
 		description: "Show available commands",
 		callback:    commandHelp,
 	},
+	"map": {
+		name:        "map",
+		description: "Show the first 20 locations in the Pokemon world",
+		callback:    commandMap,
+	},
+	"mapb": {
+		name:        "mapb",
+		description: "Show the previous 20 locations in the Pokemon world",
+		callback:    commandMapBack,
+	},
 }
 
 func main() {
+	cfg := config{
+		Next:     "https://pokeapi.co/api/v2/location-area/",
+		Previous: "",
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -35,7 +71,7 @@ func main() {
 		}
 		text := cleanInput(scanner.Text())
 		if typeofCommand, exists := cliCommands[text[0]]; exists {
-			err := typeofCommand.callback()
+			err := typeofCommand.callback(&cfg)
 			if err != nil {
 				fmt.Printf("Error executing command: %v\n", err)
 			}
@@ -45,16 +81,71 @@ func main() {
 	}
 }
 
-func commandExit() error {
+func commandExit(cfg *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
 	println("Welcome to the Pokedex!\n")
 	println("Usage:")
 	println("help: Displays a help message")
+	println("map: List the locations in the Pokemon world")
 	println("exit: Exit the Pokedex")
+	return nil
+}
+
+func commandMap(cfg *config) error {
+	res, err := http.Get(cfg.Next)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	var locationAreaResponse locationAreaResponse
+	if err := json.Unmarshal(body, &locationAreaResponse); err != nil {
+		log.Fatal(err)
+	}
+	for _, area := range locationAreaResponse.Results {
+		fmt.Printf("%s\n", area.Name)
+	}
+	cfg.Next = locationAreaResponse.Next
+	cfg.Previous = locationAreaResponse.Previous
+	return nil
+}
+
+func commandMapBack(cfg *config) error {
+	if cfg.Previous == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	res, err := http.Get(cfg.Previous)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	var locationAreaResponse locationAreaResponse
+	if err := json.Unmarshal(body, &locationAreaResponse); err != nil {
+		log.Fatal(err)
+	}
+	for _, area := range locationAreaResponse.Results {
+		fmt.Printf("%s\n", area.Name)
+	}
+	cfg.Next = locationAreaResponse.Next
+	cfg.Previous = locationAreaResponse.Previous
 	return nil
 }
